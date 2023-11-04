5f2e0505c82bdfd73ea3a77db55acbe67c185303
jabber <nmilan05@gmail.com>
2023-11-04 03:30:10 +0100

    Create app and routing

---
+++ Capstone Project/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from flask import render_template
+from model import dog_predict
+
+app = Flask(__name__)
+
+@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    img_path = request.form.get('imgurl')
+    prediction = dog_predict(img_path)
+    return prediction
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
